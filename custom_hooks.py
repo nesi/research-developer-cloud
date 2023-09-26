58890021c797027bfccf22fb04aed6b63e5635bd
Cal <35017184+CallumWalley@users.noreply.github.com>
2023-09-27 10:40:44 +1300

    Update custom_hooks.py

--- custom_hooks.py
+++ custom_hooks.py
@@ -20,18 +20,19 @@ def on_env(env, config, files, **kwargs):
     # for symbol, value in locals().items():
     #     print(symbol, value)
 
+# Some example hooks.
 
-def check_links(*args, **kwargs):
-    print("running link checker")
-    lc.check_links("docs", ext=".md", recurse=True, use_async=True)
+# def check_links(*args, **kwargs):
+#     print("running link checker")
+#     lc.check_links("docs", ext=".md", recurse=True, use_async=True)
 
 
-def lint(*args, **kwargs):
-    output = {}
-    print("running linter")
-    for file in glob.iglob("docs/**/*.md", recursive=True):
-        with open(file, "r") as f:
-            output[Path(file).stem] = pl.tools.lint(f.read())
-    with open("lint_report.json", "w+") as f:
-        f.write(json.dumps(output))
-    print(output)
+# def lint(*args, **kwargs):
+#     output = {}
+#     print("running linter")
+#     for file in glob.iglob("docs/**/*.md", recursive=True):
+#         with open(file, "r") as f:
+#             output[Path(file).stem] = pl.tools.lint(f.read())
+#     with open("lint_report.json", "w+") as f:
+#         f.write(json.dumps(output))
+#     print(output)
