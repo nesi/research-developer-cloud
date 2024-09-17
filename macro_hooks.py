fc1884b4ac056a8a92008e277e128f476a15ee50
cal <callum.walley@nesi.org.nz>
2024-09-18 11:03:11 +1200

    comment out funtions

--- macro_hooks.py
+++ macro_hooks.py
@@ -5,17 +5,19 @@ Serves a similar function to 'mkdocs_hooks' except for at render rather than tem
 import os
 import json
 
-module_list_path = os.getenv("MODULE_LIST_PATH", "docs/assets/module-list.json")
+# Uncomment this if want to have module information in template.abs
+#
+# module_list_path = os.getenv("MODULE_LIST_PATH", "docs/assets/module-list.json")
 
-def define_env(env):
-    """
-    This is the hook for defining variables, macros and filters
+# def define_env(env):
+#     """
+#     This is the hook for defining variables, macros and filters
 
-    - variables: the dictionary that contains the environment variables
-    - macro: a decorator function, to declare a macro.
-    - filter: a function with one of more arguments,
-        used to perform a transformation
-    """
+#     - variables: the dictionary that contains the environment variables
+#     - macro: a decorator function, to declare a macro.
+#     - filter: a function with one of more arguments,
+#         used to perform a transformation
+#     """
 
-    # add to the dictionary of variables available to markdown pages:
-    env.variables.applications = json.load(open(module_list_path))
+#     # add to the dictionary of variables available to markdown pages:
+#     env.variables.applications = json.load(open(module_list_path))
