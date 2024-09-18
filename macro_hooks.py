d18562f62c96981ca7a69f0a65a1b8b9d331241a
cal <callum.walley@nesi.org.nz>
2024-09-18 13:07:00 +1200

    fix hooks

--- macro_hooks.py
+++ macro_hooks.py
@@ -7,15 +7,29 @@ If this is confusing, ask Cal to explain.
 
 # Allows the use of {{ FOO }} variable in `docs/*.md`.
 
-# def define_env(env):
-#     """
-#     This is the hook for defining variables, macros and filters
 
-#     - variables: the dictionary that contains the environment variables
-#     - macro: a decorator function, to declare a macro.
-#     - filter: a function with one of more arguments,
-#         used to perform a transformation
-#     """
+def define_env(env):
+    """
+    This is the hook for defining variables, macros and filters
+
+    - variables: the dictionary that contains the environment variables
+    - macro: a decorator function, to declare a macro.
+    - filter: a function with one of more arguments,
+        used to perform a transformation
+    """
 
 #     # add to the dictionary of variables available to markdown pages:
 #     env.variables.foo = "bar"
+    return
+
+
+def on_pre_page_macros(env):
+    return
+
+
+def on_post_page_macros(env):
+    return
+
+
+def on_post_build(env):
+    return
